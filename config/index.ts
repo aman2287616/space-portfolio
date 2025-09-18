73bafefc5bf93a2104c6b8286df4392469520070
Aman Singh Yadav <amansinghyadav2287616@gmail.com>
2025-09-18 23:08:57 +0530

    javascript

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
-  title: "Abdul Rahman  | Space Portfolio",
+  title: "Aman | Space Portfolio",
   description: "Welcome to my full stack Next.js 14 space portfolio.",
   keywords: [
     "reactjs",
@@ -35,7 +35,7 @@ export const siteConfig: Metadata = {
     "css",
   ] as Array<string>,
   authors: {
-    name: "Abdul Rahman",
-    url: "https://github.com/HuzaifAhmed01",
+    name: "Aman Singh Yadav",
+    url: "https://github.com/aman2287616",
   },
 } as const;
